46f3627ce39111d46f1a2b7839315be01d7e71a0
SaRai <pasinlao2546@gmail.com>
2023-11-20 19:46:32 +0700

    2nd commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    public float initialGameSpeed = 250f; //เริ่มแรก
+    public float gameSpeedIncrease = 3f;
+    public float gameSpeed { get; private set; } //speed ล่าสุด
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            DestroyImmediate(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            Instance = null;
+        }
+    }
+
+    private void Start()
+    {
+        NewGame();
+    }
+
+    private void NewGame()
+    {
+        gameSpeed = initialGameSpeed;
+    }
+    private void Update()
+    {
+        gameSpeed += gameSpeedIncrease * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    private float leftEdge;
+    private void Start()
+    {
+        leftEdge = Camera.main.ScreenToWorldPoint(Vector3.zero).x - 2f;
+    }
+    private void Update()
+    {
+        transform.position += Vector3.left * GameManager.Instance.gameSpeed * Time.deltaTime * 0.0221f;
+
+        if(transform.position.x < leftEdge)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
