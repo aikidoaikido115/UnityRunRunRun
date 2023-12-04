46064e748a1a13d781907daf015aff906104054d
SaRai <74460961+aikidoaikido115@users.noreply.github.com>
2023-12-04 20:53:15 +0700

    Add files via upload

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
 
 public class GameManager : MonoBehaviour
 {
@@ -9,6 +12,12 @@ public class GameManager : MonoBehaviour
     public float initialGameSpeed = 250f; //เริ่มแรก
     public float gameSpeedIncrease = 3f;
     public float gameSpeed { get; private set; } //speed ล่าสุด
+
+    public TextMeshProUGUI gameOverText;
+    public Button retryButton;
+
+    private Player player;
+    private Spawner spawner;
 
 
     private void Awake()
@@ -33,13 +42,42 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
+        player = FindObjectOfType<Player>();
+        spawner = FindObjectOfType<Spawner>();
+
         NewGame();
     }
 
-    private void NewGame()
+    public void NewGame()
     {
+        Obstacle[] obstacles = FindObjectsOfType<Obstacle>();
+
+        foreach(var obstacle in obstacles)
+        {
+            Destroy(obstacle.gameObject);
+        }
+
         gameSpeed = initialGameSpeed;
+        enabled = true;
+
+        player.gameObject.SetActive(true);
+        spawner.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(false);
+        retryButton.gameObject.SetActive(false);
     }
+
+    public void GameOver()
+    {
+
+        gameSpeed = 0f;
+        enabled = false;
+
+        player.gameObject.SetActive(false);
+        spawner.gameObject.SetActive(false);
+        gameOverText.gameObject.SetActive(true);
+        retryButton.gameObject.SetActive(true);
+    }
+
     private void Update()
     {
         gameSpeed += gameSpeedIncrease * Time.deltaTime;
