a8b8c6a3f833e1bfc57ba0c3a920e8733a6b0935
SaRai <pasinlao2546@gmail.com>
2023-12-08 20:14:30 +0700

    Test commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    public float initialGameSpeed = 250f; //เริ่มแรก
+    public float gameSpeedIncrease = 3f;
+    public float gameSpeed { get; set; } //speed ล่าสุด
+    public float score;
+    public float scoreMultiplier; // มีผลกับเอฟเฟกต์ DoubleScore
+    public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI currentScoreText;
+    public TextMeshProUGUI highScoreText;
+    public Button retryButton;
+
+    private Player player;
+    private Spawner spawner;
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            DestroyImmediate(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            Instance = null;
+        }
+    }
+
+    private void Start()
+    {
+        player = FindObjectOfType<Player>();
+        spawner = FindObjectOfType<Spawner>();
+        
+        NewGame();
+    }
+
+    public void NewGame()
+    {
+        Obstacle[] obstacles = FindObjectsOfType<Obstacle>();
+
+        foreach(var obstacle in obstacles)
+        {
+            Destroy(obstacle.gameObject);
+        }
+
+        score = 0;
+        scoreMultiplier = 1;
+        gameSpeed = initialGameSpeed;
+        enabled = true;
+
+        player.gameObject.SetActive(true);
+        spawner.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(false);
+        retryButton.gameObject.SetActive(false);
+
+        UpdateHiscore();
+    }
+
+    public void GameOver()
+    {
+
+        gameSpeed = 0f;
+        enabled = false;
+
+        player.gameObject.SetActive(false);
+        spawner.gameObject.SetActive(false);
+        gameOverText.gameObject.SetActive(true);
+        retryButton.gameObject.SetActive(true);
+
+        UpdateHiscore();
+    }
+
+    private void Update()
+    {
+        gameSpeed += gameSpeedIncrease * Time.deltaTime;
+        score += gameSpeed * Time.deltaTime * scoreMultiplier;
+        currentScoreText.text = Mathf.FloorToInt(score).ToString("D8");
+    }
+
+    private void UpdateHiscore()
+    {
+        float hiscore = PlayerPrefs.GetFloat("hiscore", 0);
+
+        if (score > hiscore)
+        {
+            hiscore = score;
+            PlayerPrefs.SetFloat("hiscore", hiscore);
+        }
+
+        highScoreText.text = Mathf.FloorToInt(hiscore).ToString("D8");
+    }
+}
+
+
+//test
